28a2951b5fb6906e28dd29473d19ef56c9e2e3d5
Cameron Carper <108481991+acousticarpal@users.noreply.github.com>
2025-11-10 08:47:22 -0600

    server side log statement

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log(`${new Date().toISOString()} : Returning Date() in JSON Format`);
   res.json({
     date: new Date().toISOString(),
   })
